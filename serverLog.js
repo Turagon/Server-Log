596e3800dac71a47f7c89752d36966459174f826
rexlin <rexlin6245@gmail.com>
2021-06-14 22:12:17 -0700

    create server environment & build middleware --> serverLog.js

---
+++ serverLog.js
@@ -0,0 +1,15 @@
+function serverLog (req, res, next) {
+  const method = req.method
+  const url = req.originalUrl
+  const host = req.headers.host
+  req.time = new Date()
+  const timeDifference = function () {
+    const resTime = new Date()
+    console.log(req.time.toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }), " | ", method, "from", host + url, " | ", "total time : ", resTime.getTime() - req.time.getTime(), "ms")
+  }
+  res.once('finish', timeDifference)
+  res.once('close', timeDifference)
+  return next()
+}
+
+module.exports = serverLog
